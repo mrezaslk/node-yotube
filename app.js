5ae60e6c7ef36ab5e4d96e0c1669a3079e89bc33
mohammadreza salek <rezasalek75@gmail.com>
2023-07-14 18:29:36 +0330

    invoke function in module

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Modules
 const { ali, john } = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
 
-sayHi("reza")
-sayHi(john)
-sayHi(ali)
+require('./7-mind-gerenade');
+
+// sayHi("reza")
+// sayHi(john)
+// sayHi(ali)
