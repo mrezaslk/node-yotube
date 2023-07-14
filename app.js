3e48962e60b258f68d7ef30f79dde226c1972204
mohammadreza salek <rezasalek75@gmail.com>
2023-07-14 18:01:12 +0330

    GLOBALS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const amount = 9;
+// GLOBALS = NO WINDOW !!!
 
-if (amount < 10) {
-    console.log("small number");
-}
-else {
-    console.log("large number");
-}
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
 
-// console.log(`${amount} hey it's my first node app!!!`);
+
+console.log(__dirname);
+setInterval(() => {
+    console.log("hello world");
+}, 100)
